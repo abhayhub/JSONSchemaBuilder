a8ebdaeebad2a6aa932f21dea7ed2c82c7e9daf5
Abhay Tiwari <abhayskp7948@gmail.com>
2025-07-20 13:29:09 +0530

    Implemented logic to create JSON schema

--- src/components/JsonBuilder.tsx
+++ src/components/JsonBuilder.tsx
@@ -34,10 +34,26 @@ const updateField = (idx: number, updated: Field) => {
   setValues(newValues);
 };
 
+// Generate json
+const CreateJSON = (fields: Field[]): any => {
+
+  const Output: any = {};
+
+  fields.forEach((v) => {
+    if (!v.name) return;
+    if (v.type === "nested") {
+      Output[v.name] = CreateJSON(v.children || []);
+    } else {
+      Output[v.name] = v.type === "string" ? "STRING" : "number";
+    }
+  });
+  return Output;
+};
+
   return (
     <div className="flex min-h-screen">
       <div className="w-1/2 p-6">
-        <h2 className="text-xl font-semibold mb-4">Schema Builder</h2>
+        <h2 className="text-xl font-semibold mb-4">Model Maker</h2>
         {values.map((field, idx) => (
           <FieldItem
           key={field.id}
@@ -51,10 +67,10 @@ const updateField = (idx: number, updated: Field) => {
       
       {/* ouput section */}
       <div className="w-1/2 p-6 bg-gray-100">
-        <h2 className="text-xl font-semibold mb-4">Live JSON Preview</h2>
+        <h2 className="text-xl font-semibold mb-4">JSON Preview</h2>
         <Card className="p-4">
           <pre className="text-sm text-gray-800 whitespace-pre-wrap">
-            {}
+            {JSON.stringify(CreateJSON(values), null, 2)}
           </pre>
         </Card>
       </div>
